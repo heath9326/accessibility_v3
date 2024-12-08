1ab05a2ea58129de30004626813bcce3ef7f2f94
Evgeniya Seminenko <Heathwindart@gmail.com>
2024-12-08 19:31:52 +0300

    main.py refactoring

--- assessement/utils.py
+++ assessement/utils.py
@@ -1,6 +1,5 @@
 
 def count_syllables(word: str) -> int:
-    # Get vowel sounds in the word
     last_letter = [*word][-1]
     vowels = ["а", "у", "о", "и", "э", "ы", "е", "ё", "ю", "я"]
     num_syllables = 0
@@ -13,3 +12,13 @@ def count_syllables(word: str) -> int:
             num_syllables += 1
         prev_char = char
     return num_syllables
+
+
+def load_texts():
+    with open('dataset.txt', mode='r') as file:
+        texts = []
+        all_texts = file.read().split('\n\n')  # Split by empty lines
+
+        for text_block in all_texts:
+            texts.append(text_block)#
+        return texts
--- config.py
+++ config.py
@@ -1,4 +1,9 @@
+from fastapi import FastAPI
 from pydantic_settings import BaseSettings
+import logging
+import logging.config
+
+from starlette.templating import Jinja2Templates
 
 
 class Settings(BaseSettings):
@@ -6,4 +11,43 @@ class Settings(BaseSettings):
     word_complexity_index: int = 4
 
 
+LOGGING_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "default": {
+            "format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        },
+        "detailed": {
+            "format": "%(asctime)s - %(name)s - %(levelname)s - [%(filename)s:%(lineno)d] - %(message)s",
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "default",
+        },
+        "file": {
+            "class": "logging.FileHandler",
+            "filename": "app.log",
+            "formatter": "detailed",
+        },
+    },
+    "loggers": {
+        "fastapi": {
+            "handlers": ["console", "file"],
+            "level": "INFO",
+        },
+        "my_app": {
+            "handlers": ["console", "file"],
+            "level": "DEBUG",
+            "propagate": False,
+        },
+    },
+}
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+logging.config.dictConfig(LOGGING_CONFIG)
+logger = logging.getLogger("my_app")
 settings = Settings()
